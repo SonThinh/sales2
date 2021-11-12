a5e6c2976dd1af6645a52a9bcbf4c1af76b68be9
Son Thinh <thinh.pham@gumiviet.com>
2021-11-12 10:13:36 +0700

    fix(role): remove email in role filter

--- app/Filters/RoleFilter.php
+++ app/Filters/RoleFilter.php
@@ -9,17 +9,6 @@ class RoleFilter extends Filter
 {
     use CommonFilter;
 
-    /**
-     * Filter user by email
-     *
-     * @param string $email
-     * @return \App\Builders\Builder
-     */
-    public function email($email)
-    {
-        return $this->query->whereLike('email', $email);
-    }
-
     public function name($name)
     {
         return $this->query->whereLike('name', $name);
