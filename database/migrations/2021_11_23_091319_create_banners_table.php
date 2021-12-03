e54de1afd08300551269cdd5cc5635ffae886a98
ADMIN\CONG DAT <dat.tran@gumiviet.com>
2021-12-03 14:32:15 +0700

    fix(CRUD-product): add banner url

--- database/migrations/2021_11_23_091319_create_banners_table.php
+++ database/migrations/2021_11_23_091319_create_banners_table.php
@@ -16,6 +16,7 @@ class CreateBannersTable extends Migration
         Schema::create('banners', function (Blueprint $table) {
             $table->id();
             $table->string('banner_name');
+            $table->string('banner_url');
             $table->timestamps();
         });
     }
